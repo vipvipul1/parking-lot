39dc54a36f60dd2d467218dd99c35262404736a1
Vipul Kumar <vipvipul1@gmail.com>
2023-08-04 20:28:44 +0530

    initial commit

---
+++ src/main/java/org/vip/ParkingLotApp.java
@@ -0,0 +1,13 @@
+package org.vip;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParkingLotApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/vip/ParkingLotAppTest.java
@@ -0,0 +1,38 @@
+package org.vip;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParkingLotAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ParkingLotAppTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ParkingLotAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
